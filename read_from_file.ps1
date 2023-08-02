a43fe506417c87674f71368342b8419402c4255d
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2023-08-02 14:07:47 +0800

    Update read_from_file.ps1

--- read_from_file.ps1
+++ read_from_file.ps1
@@ -1,2 +1,9 @@
-New-Item D:\temp\test\test.txt
-\[string[]]$arrayFromFile = Get-Content -Path 'C:\USER\Documents\Collections\collection.txt
+#$PSVersionTable
+#New-Item -Path "c:\" -Name "logfiles" -ItemType "directory"
+#New-Item -Path $profile -ItemType "file" -Force
+[string[]]$arrayFromFile = Get-Content -Path 'C:\Users\tiger\Desktop\notepad++backup\new.txt'
+for($i=0;$i -le $arrayFromFile.Length-1 ; $i++){ 
+   nslookup.exe $arrayFromFile[$i];
+    }
+
+Read-Host -prompt "wait"
